53d7ed92515559e8f66665b15b35f65394fd1821
Benjamin Doherty <bjd@bangpound.org>
2013-09-23 20:24:53 -0500

    initial commit.

---
+++ LadybugServiceProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Bangpound\Silex;
+
+use Silex\Application;
+use Silex\ServiceProviderInterface;
+
+class LadybugServiceProvider implements ServiceProviderInterface
+{
+    /**
+     * Registers services on the given app.
+     *
+     * This method should only be used to configure services and parameters.
+     * It should not get services.
+     *
+     * @param Application $app An Application instance
+     */
+    public function register(Application $app)
+    {
+        $app['ladybug'] = $app->share(function () use ($app) {
+            return new \Ladybug\Dumper();
+        });
+    }
+
+    /**
+     * Bootstraps the application.
+     *
+     * This method is called after all services are registered
+     * and should be used for "dynamic" configuration (whenever
+     * a service must be requested).
+     */
+    public function boot(Application $app)
+    {
+        // TODO: Implement boot() method.
+    }
+}
